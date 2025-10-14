1298f17f4c6649e85123ef586a72b08e32022f4f
Daniel Rivas <danielrivasmd@gmail.com>
2025-10-14 13:59:57 +0300

    add params config

---
+++ src/config/params.jl
@@ -0,0 +1,28 @@
+####################################################################################################
+
+using Parameters: @with_kw
+
+####################################################################################################
+
+@with_kw mutable struct CNNParams
+    batchsize::Int           = 64           # dataloader batch size
+    shuffle::Bool            = true         # dataloader shuffle
+
+    device::Function         = gpu          # gpu or cpu
+    kernelsize::Int          = 5            # convulution kernel size
+    epochs::Int              = 50           # number of epochs
+
+    σ::Function              = relu         # activation function
+    maxpool::Int             = 2            # max pooling
+    η::Float64               = 1e-2         # learning rate
+    momemntum::Float64       = 0.9          # optimizer momentum
+    dropout1::Float64        = 0.2          # block 1 dropout
+    dropout2::Float64        = 0.3          # block 2 dropout
+    dropout3::Float64        = 0.4          # block 3 dropout
+    dropout_dense::Float64   = 0.5          # head dropout
+    layerout1::Int           = 32           # neuron number out block 1
+    layerout2::Int           = 64           # neuron number out block 2
+    layerout3::Int           = 128          # neuron number out block 3
+end
+
+####################################################################################################
