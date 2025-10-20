6e01320178b299caf493fb2e2f7a7487d27b12a3
Daniel Rivas <danielrivasmd@gmail.com>
2025-10-20 13:30:29 +0300

    update paths

--- src/config/paths.jl
+++ src/config/paths.jl
@@ -2,30 +2,40 @@
 
 module Paths
 
-"Project root"
+"project root"
 const PROJECT = normpath(joinpath(@__DIR__, "..", ".."))
 
-"Data"
+"data"
 const DATA = joinpath(PROJECT, "data")
+const BAM = joinpath(DATA, "bam")
 const FASTA = joinpath(DATA, "fasta")
+const INFERENCE = joinpath(DATA, "inference")
 
-"Graph"
+"graph"
 const GRAPH = joinpath(PROJECT, "graph")
+const PERFORMANCE = joinpath(GRAPH, "performance")
 
-"Model"
+"model"
 const MODEL = joinpath(PROJECT, "model")
 
-"Source"
+# sh: only called from shell
+
+"src"
 const SRC = joinpath(PROJECT, "src")
 const CONFIG = joinpath(SRC, "config")
 const UTIL = joinpath(SRC, "util")
 const BIN = joinpath(SRC, "bin")
 
+"sysimage"
+const SYSIMAGE = joinpath(PROJECT, "sysimage")
+
+# toml: only called from shell
+
 "Ensure directories exist (for outputs)"
 function ensure_dirs()
-  for d in (DATA, FASTA, GRAPH, MODEL)
-    isdir(d) || mkpath(d)
-  end
+    for d in (DATA, BAM, FASTA, INFERENCE, GRAPH, PERFORMANCE, MODEL, SYSIMAGE)
+        isdir(d) || mkpath(d)
+    end
 end
 
 end
