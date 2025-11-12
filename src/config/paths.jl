fab606e45990e1b29aad8bcb1a86459db7ee211c
Daniel Rivas <danielrivasmd@gmail.com>
2025-11-12 14:49:28 +0200

    update paths

--- src/config/paths.jl
+++ src/config/paths.jl
@@ -14,6 +14,7 @@ const INFERENCE = joinpath(DATA, "inference")
 "graph"
 const GRAPH = joinpath(PROJECT, "graph")
 const PERFORMANCE = joinpath(GRAPH, "performance")
+const ROC = joinpath(GRAPH, "roc")
 
 "model"
 const MODEL = joinpath(PROJECT, "model")
@@ -26,14 +27,11 @@ const CONFIG = joinpath(SRC, "config")
 const UTIL = joinpath(SRC, "util")
 const BIN = joinpath(SRC, "bin")
 
-"sysimage"
-const SYSIMAGE = joinpath(PROJECT, "sysimage")
-
 # toml: only called from shell
 
 "Ensure directories exist (for outputs)"
 function ensure_dirs()
-    for d in (DATA, BAM, FASTA, INFERENCE, GRAPH, PERFORMANCE, MODEL, SYSIMAGE)
+    for d in (DATA, BAM, FASTA, INFERENCE, GRAPH, PERFORMANCE, ROC, MODEL)
         isdir(d) || mkpath(d)
     end
 end
