cd9b848eccdc99ddc2865e78a65d4359772ef103
Daniel Rivas <danielrivasmd@gmail.com>
2025-11-13 14:41:49 +0200

    patch simulation

--- sh/pipe/simulation.sh
+++ sh/pipe/simulation.sh
@@ -1,15 +1,15 @@
-Ns="10000 20000 30000 40000 50000 100000 200000 300000 400000 500000"
+Ns=(10000 20000 30000 40000 50000 100000 200000 300000 400000 500000)
 L=75
 
 for N in $Ns
 do
-echo "Running gargammel.sh with N=$N and L=$L"
+echo "Running gargammel.sh with N=$N & L=$L"
 ./sh/bin/gargammel.sh "$N" "$L"
 done
 
 for f in simul/h38_ancient_*_75.b.fa.gz
 do
- out="${f%.b.fa.gz}.fasta"
+ out="${f%.b.fa.gz}nt.fasta"
  echo "Unzipping $f -> $out"
  gunzip -c "$f" > "$out"
 done
@@ -18,7 +18,7 @@ rm simul/h38_ancient_*.f[aq].gz
 
 for N in $Ns
 do
-OUT="simul/h38_modern_${N}_${L}.fasta"
+OUT="simul/h38_modern_${N}_${L}nt.fasta"
 
 echo "Running genomeExtract.jl for N=$N and L=$L -> $OUT"
 julia --project src/bin/genomeExtract.jl data/refseq/GCF_000001405.26_GRCh38_genomic.fna "$OUT"
