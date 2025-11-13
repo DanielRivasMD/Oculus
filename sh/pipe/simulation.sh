ba90f110b666b8721cba4d41023a65f56a57a77d
Daniel Rivas <danielrivasmd@gmail.com>
2025-11-13 15:44:57 +0200

    patch simulation pipe

--- sh/pipe/simulation.sh
+++ sh/pipe/simulation.sh
@@ -1,4 +1,4 @@
-Ns=(10000 20000 30000 40000 50000 100000 200000 300000 400000 500000)
+Ns=(10000 20000 30000 40000 50000 100000 200000 300000 400000 500000 1000000 5000000 10000000 50000000 100000000)
 L=75
 
 for N in $Ns
@@ -21,5 +21,5 @@ do
 OUT="simul/h38_modern_${N}_${L}nt.fasta"
 
 echo "Running genomeExtract.jl for N=$N and L=$L -> $OUT"
-julia --project src/bin/genomeExtract.jl data/refseq/GCF_000001405.26_GRCh38_genomic.fna "$OUT"
+julia --project src/bin/genomeExtract.jl data/refseq/GCF_000001405.26_GRCh38_genomic.fna "$N" "$L" "$OUT"
 done
