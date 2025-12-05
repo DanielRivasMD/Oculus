8d7bd64daf8968aeabe389e595b3a33d1d9deaba
Daniel Rivas <danielrivasmd@gmail.com>
2025-12-05 15:19:22 +0200

    patch simulation pipe

--- sh/pipe/simulation.sh
+++ sh/pipe/simulation.sh
@@ -1,5 +1,6 @@
-Ns=(10000 20000 30000 40000 50000 100000 200000 300000 400000 500000 1000000 5000000 10000000 50000000 100000000)
-L=75
+# Ns=(10000 20000 30000 40000 50000 100000 200000 300000 400000 500000 1000000 5000000 10000000 50000000 100000000)
+Ns=(100000)
+L=76
 
 for N in $Ns
 do
@@ -7,7 +8,7 @@ echo "Running gargammel.sh with N=$N & L=$L"
 ./sh/bin/gargammel.sh "$N" "$L"
 done
 
-for f in simul/h38_ancient_*_75.b.fa.gz
+for f in simul/h38_ancient_*.b.fa.gz
 do
  out="${f%.b.fa.gz}nt.fasta"
  echo "Unzipping $f -> $out"
