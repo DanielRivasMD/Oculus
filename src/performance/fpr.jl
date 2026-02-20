c34e934e9984705cb3c57b89876b2d1a6163f6e0
Daniel Rivas <danielrivasmd@gmail.com>
2026-02-20 17:07:46 +0200

    import false positive rate

---
+++ src/performance/fpr.jl
@@ -0,0 +1,31 @@
+####################################################################################################
+
+"""
+
+    FPR(ɒ::M)
+      where M <: Matrix{N}
+      where N <: Number
+
+# Description
+Calculate False Positive Rate (FPR) from contingency table or confusion matrix.
+Also called Fall-out.
+
+# Examples
+```jldoctest
+julia> χ = [20 180; 10 1820]
+julia> FPR(χ)
+0.09
+```
+
+
+See also: [`performance`](@ref), [`accuracy`](@ref), [`balancedAccuracy`](@ref), [`fScore`](@ref), [`sensitivity`](@ref), [`specificity`](@ref), [`PPV`](@ref), [`NPV`](@ref), [`FPR`](@ref), [`FNR`](@ref), [`FDR`](@ref), [`FOR`](@ref), [`MCC`](@ref).
+"""
+function FPR(ɒ::M) where M <: Matrix{N} where N <: Number
+  if size(ɒ) == (2, 2)
+    return ɒ[1, 2] / (ɒ[1, 2] + ɒ[2, 2])
+  else
+    @error "Array does not have the proper size"
+  end
+end
+
+####################################################################################################
