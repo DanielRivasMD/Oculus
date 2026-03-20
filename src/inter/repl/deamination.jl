1a4add1215887c601320c5541757fde005ad5cfc
Daniel Rivas <danielrivasmd@gmail.com>
2026-03-20 16:14:42 +0200

    add deamination repl

---
+++ src/inter/repl/deamination.jl
@@ -0,0 +1,41 @@
+module DAREPL
+
+using Avicenna.Flow: Cache, run
+using ..DAFlow: deamination_workflow
+using ..DACore: fname, compare_fasta_files, plot_composition
+
+export run_deamination, compare_fasta_files, plot_composition
+
+"""
+    run_deamination(
+      modern::String,
+      ancient::String,
+      csv::String="out.csv",
+      png::String="out.png",
+      verbose::Bool=false,
+      no_cache::Bool=false) -> Result
+
+Run the deamination analysis workflow from the REPL.
+"""
+function run_deamination(
+  modern::String,
+  ancient::String;
+  csv::String = "out.csv",
+  png::String = "out.png",
+  verbose::Bool = false,
+  no_cache::Bool = false,
+)
+  config = Dict{String,Any}(
+    "modern" => modern,
+    "ancient" => ancient,
+    "csv" => csv,
+    "png" => png,
+    "verbose" => verbose,
+    "modern_name" => fname(modern),
+    "ancient_name" => fname(ancient),
+  )
+  cache = Cache("cache/deamination", !no_cache)
+  return run(deamination_workflow, config, cache = cache)
+end
+
+end
