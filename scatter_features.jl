237636106a0b34922d54c4da8ac24665dda3fdfb
Daniel Rivas <danielrivasmd@gmail.com>
2026-03-24 16:38:29 +0200

    add scatter features

---
+++ scatter_features.jl
@@ -0,0 +1,47 @@
+using DataFrames
+using DelimitedFiles
+using Plots
+
+function readdf(path; sep = ',')
+  data, header = readdlm(path, sep, header = true)
+  return DataFrame(data, vec(header))
+end
+
+df = readdf("data/cache/features/engineered.csv")
+
+p1 = scatter(
+  df.ga3p_5,
+  df.ct5p_5,
+  group = df.label,
+  alpha = 0.3,
+  xlabel = "ga3p_5",
+  ylabel = "ct5p_5",
+  title = "ga3p_5 vs ct5p_5 colored by label",
+  legend = true,
+  legendtitle = "Label",
+  labels = ["Ancient (0)" "Modern (1)"],
+)
+
+p2 = scatter(
+  df.ga3p_5[df.label.==0],
+  df.ct5p_5[df.label.==0],
+  color = :blue,
+  alpha = 0.3,
+  xlabel = "ga3p_5",
+  ylabel = "ct5p_5",
+  title = "Ancient only",
+  legend = false,
+)
+
+p3 = scatter(
+  df.ga3p_5[df.label.==1],
+  df.ct5p_5[df.label.==1],
+  color = :red,
+  alpha = 0.3,
+  xlabel = "ga3p_5",
+  ylabel = "ct5p_5",
+  title = "Modern only",
+  legend = false,
+)
+
+plot(p1, p2, p3, layout = (3, 1), size = (1200, 400))
