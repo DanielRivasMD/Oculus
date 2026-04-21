7504cba5f307490db139b28e1cc289f62c64b64a
Daniel Rivas <danielrivasmd@gmail.com>
2026-04-21 13:00:52 +0300

    update feature flow

--- src/flow/flow_features.jl
+++ src/flow/flow_features.jl
@@ -9,31 +9,31 @@ using ..FECore
 
 ####################################################################################################
 
-export features_flow
+export flow
 
 ####################################################################################################
 
-const features_flow = Config(
+const flow = Config(
   "feature_extraction",
   [
-    Stage("load_modern", (config, _) -> FECore.load_fasta(config["modern"]), "1.0"),
-    Stage("load_ancient", (config, _) -> FECore.load_fasta(config["ancient"]), "1.0"),
+    Stage("01_load_ancient", (config, _) -> FECore.load_fasta(config["ancient"]), "1.0"),
+    Stage("01_load_modern", (config, _) -> FECore.load_fasta(config["modern"]), "1.0"),
     Stage(
-      "build_modern",
+      "02_build_ancient",
       (config, prev) ->
-        FECore.build_df(prev["load_modern"], 1; onehot = config["onehot"]),
+        FECore.build_df(prev["01_load_ancient"], 0; onehot = config["onehot"]),
       "1.0",
     ),
     Stage(
-      "build_ancient",
+      "02_build_modern",
       (config, prev) ->
-        FECore.build_df(prev["load_ancient"], 0; onehot = config["onehot"]),
+        FECore.build_df(prev["01_load_modern"], 1; onehot = config["onehot"]),
       "1.0",
     ),
-    Stage("merge", (_, prev) -> vcat(prev["build_modern"], prev["build_ancient"]), "1.0"),
+    Stage("03_merge", (_, prev) -> vcat(prev["02_build_ancient"], prev["02_build_modern"]), "1.0"),
     Stage(
-      "write",
-      (config, prev) -> FECore.writedf(config["out"], prev["merge"]; sep = ','),
+      "04_write",
+      (config, prev) -> FECore.writedf(config["out"], prev["03_merge"]; sep = ','),
       "1.0",
     ),
   ],
