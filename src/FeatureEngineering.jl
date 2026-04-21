3dea6adb4ed7f4ee072a29f1aa28eac7fac1c578
Daniel Rivas <danielrivasmd@gmail.com>
2026-04-21 22:13:10 +0300

    update feature engineering

--- src/FeatureEngineering.jl
+++ src/FeatureEngineering.jl
@@ -4,10 +4,10 @@ module FE
 
 ####################################################################################################
 
-include("util/util_features.jl")
-include("flow/flow_features.jl")
-include("inter/repl/repl_features.jl")
-include("inter/cli/cli_features.jl")
+include("util/feutil.jl")
+include("flow/feflow.jl")
+include("inter/repl/ferepl.jl")
+include("inter/cli/fecli.jl")
 
 ####################################################################################################
 
