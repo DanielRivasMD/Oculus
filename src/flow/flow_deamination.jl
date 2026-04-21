605c0b73190ad40249ea1d2014a1793b963af3a6
Daniel Rivas <danielrivasmd@gmail.com>
2026-04-21 13:11:24 +0300

    update deamination flow

--- src/flow/flow_deamination.jl
+++ src/flow/flow_deamination.jl
@@ -1,37 +1,45 @@
+####################################################################################################
+
 module DAFlow
+
+####################################################################################################
 
 using Avicenna.Flow: Stage, Config
 using ..DACore
 
-export deamination_flow
+####################################################################################################
 
-const deamination_flow = Config(
+export flow
+
+####################################################################################################
+
+const flow = Config(
   "deamination_analysis",
   [
-    Stage("load_modern", (config, _) -> DACore.load_fasta(config["modern"]), "1.0"),
-    Stage("load_ancient", (config, _) -> DACore.load_fasta(config["ancient"]), "1.0"),
+    Stage("01_load_ancient", (config, _) -> DACore.load_fasta(config["ancient"]), "1.0"),
+    Stage("01_load_modern", (config, _) -> DACore.load_fasta(config["modern"]), "1.0"),
     Stage(
-      "compute_composition",
+      "02_compute_composition",
       (config, prev) -> begin
-        comp_modern = DACore.position_composition(prev["load_modern"])
-        comp_ancient = DACore.position_composition(prev["load_ancient"])
+        comp_ancient = DACore.position_composition(prev["01_load_ancient"])
+        comp_modern = DACore.position_composition(prev["01_load_modern"])
         return (comp_modern, comp_ancient)
       end,
       "1.0",
     ),
     Stage(
-      "write_csv",
+      "03_write_csv",
       (config, prev) -> DACore.write_csv(
         config["csv"],
-        config["modern_name"],
         config["ancient_name"],
-        prev["compute_composition"][1],
-        prev["compute_composition"][2],
+        config["modern_name"],
+        prev["02_compute_composition"][1],
+        prev["02_compute_composition"][2],
       ),
       "1.0",
     ),
     Stage(
-      "plot",
+      "04_plot",
       (config, prev) -> DACore.plot_composition(config["csv"]; outfile = config["png"]),
       "1.0",
     ),
@@ -39,4 +47,8 @@ const deamination_flow = Config(
   "1.0",
 )
 
+####################################################################################################
+
 end
+
+####################################################################################################
