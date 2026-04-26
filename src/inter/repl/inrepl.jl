28450d4923939d644ceda57f4652d330e110093b
Daniel Rivas <danielrivasmd@gmail.com>
2026-04-26 13:38:54 +0300

    add inference repl

---
+++ src/inter/repl/inrepl.jl
@@ -0,0 +1,29 @@
+module INREPL
+
+using Avicenna.Flow: Cache, launch
+using ..INFlow: flow
+
+export run
+
+"""
+    run(;
+        model::String,
+        data::String,
+        out::String = "predictions.csv",
+        no_cache::Bool = false,
+    ) -> Result
+
+Run the inference workflow from the REPL.
+"""
+function run(;
+  model::String,
+  data::String,
+  out::String = "predictions.csv",
+  no_cache::Bool = false,
+)
+  config = Dict{String,Any}("model" => model, "data" => data, "out" => out)
+  cache = Cache("cache/inference", !no_cache)
+  return launch(flow, config, cache = cache)
+end
+
+end
