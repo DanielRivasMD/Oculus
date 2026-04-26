b033ba0ab4e70f67e23feeaf12704f759bc42caf
Daniel Rivas <danielrivasmd@gmail.com>
2026-04-26 13:26:14 +0300

    add neural network flow

---
+++ src/flow/nnflow.jl
@@ -0,0 +1,34 @@
+module NNFlow
+
+using Avicenna.Flow: Stage, Config
+using ..NNCore
+
+export flow
+
+const flow = Config(
+  "neural_network_training",
+  [
+    Stage(
+      "01_load_configs",
+      (config, _) -> begin
+        hparams = NNCore.loadHparams(config["cnn"])
+        sparams = NNCore.loadSparams(config["sample"])
+        return (hparams = hparams, sparams = sparams)
+      end,
+      "1.0",
+    ),
+    Stage(
+      "02_train",
+      (config, prev) -> begin
+        hparams = prev["01_load_configs"].hparams
+        sparams = prev["01_load_configs"].sparams
+        out_base = config["out"]
+        return NNCore.train_and_save(hparams, sparams, out_base)
+      end,
+      "1.0",
+    ),
+  ],
+  "1.0",
+)
+
+end
