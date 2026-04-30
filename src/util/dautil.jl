32175ac27c58645ad4ce9a8fd0a05693ba8f8bfe
Daniel Rivas <danielrivasmd@gmail.com>
2026-04-30 07:30:51 +0300

    update deamination util

--- src/util/dautil.jl
+++ src/util/dautil.jl
@@ -6,11 +6,13 @@ module DACore
 
 using DelimitedFiles
 using Plots
-using FilePathsBase: splitpath
+using FilePathsBase: splitpath, basename
+using ..USCore: load_fasta
 
 ####################################################################################################
 
-export position_composition, write_csv, plot_composition, compare_fasta_files, fname
+export position_composition,
+  write_csv_single, write_csv_dual, plot_composition, compare_fasta_files, fname
 
 ####################################################################################################
 
@@ -55,7 +57,37 @@ end
 
 ####################################################################################################
 
-function write_csv(
+"""
+    write_csv_single(outpath::String, comp::Vector{Dict{Char,Float64}})
+
+Write a CSV with columns: position, A, T, G, C
+"""
+function write_csv_single(outpath::String, comp::Vector{Dict{Char,Float64}})
+  L = length(comp)
+  data = Matrix{Float64}(undef, L, 5)
+  for i = 1:L
+    data[i, 1] = i
+    data[i, 2] = comp[i]['A']
+    data[i, 3] = comp[i]['T']
+    data[i, 4] = comp[i]['G']
+    data[i, 5] = comp[i]['C']
+  end
+  header = hcat("position", "A", "T", "G", "C")
+  writedlm(outpath, vcat(header, data), ',')
+end
+
+####################################################################################################
+
+"""
+    write_csv_dual(outpath::String,
+                   modern_name::String,
+                   ancient_name::String,
+                   comp1::Vector{Dict{Char,Float64}},
+                   comp2::Vector{Dict{Char,Float64}})
+
+Write the combined CSV for two‑file comparison
+"""
+function write_csv_dual(
   outpath::String,
   modern_name::String,
   ancient_name::String,
@@ -76,7 +108,6 @@ function write_csv(
     data[i, 9] = comp2[i]['C']
   end
 
-  # Create a 1×9 row matrix for the header
   header = hcat(
     "position",
     modern_name * "_A",
@@ -97,17 +128,31 @@ end
 """
     plot_composition(csvfile::String; outfile::Union{Nothing,String}=nothing)
 
-Generate a line plot of per‑position base composition from the CSV file
+Generate a line plot of per‑position base composition
+Detects single‑file (5 columns) vs two‑file (9 columns) format automatically
 """
 function plot_composition(csvfile::String; outfile::Union{Nothing,String} = nothing)
   raw = readdlm(csvfile, ',', String)
+  header = raw[1, :]
   data = raw[2:end, :]
   positions = parse.(Float64, data[:, 1])
 
-  modern_label = "Modern"
-  ancient_label = "Ancient"
-  colors = Dict("A" => "green", "T" => "red", "G" => "orange", "C" => "blue")
+  ncols = size(data, 2)
+  if ncols == 5   # single file
+    groups = [("Sample", :solid)]
+    bases = ["A", "T", "G", "C"]
+    cols_offset = 1
+  elseif ncols == 9   # two files
+    modern_name = replace(header[2], r"_A$" => "")
+    ancient_name = replace(header[6], r"_A$" => "")
+    groups = [(modern_name, :solid), (ancient_name, :dash)]
+    bases = ["A", "T", "G", "C"]
+    cols_offset = 1
+  else
+    error("Unsupported CSV format: expected 5 or 9 columns")
+  end
 
+  colors = Dict("A" => "green", "T" => "red", "G" => "orange", "C" => "blue")
   plt = plot(
     ylim = (15, 35),
     size = (1000, 600),
@@ -116,30 +161,19 @@ function plot_composition(csvfile::String; outfile::Union{Nothing,String} = noth
     title = "Base Composition Along Sequence",
   )
 
-  # Modern (solid)
-  for (j, base) in enumerate(["A", "T", "G", "C"])
-    col = parse.(Float64, data[:, 1+j])
-    plot!(
-      plt,
-      positions,
-      col,
-      label = "$modern_label $base",
-      color = colors[base],
-      linestyle = :solid,
-    )
-  end
-
-  # Ancient (dashed)
-  for (j, base) in enumerate(["A", "T", "G", "C"])
-    col = parse.(Float64, data[:, 5+j])
-    plot!(
-      plt,
-      positions,
-      col,
-      label = "$ancient_label $base",
-      color = colors[base],
-      linestyle = :dash,
-    )
+  for (group_name, linestyle) in groups
+    for (j, base) in enumerate(bases)
+      col_idx = cols_offset + (groups[1] == (group_name, linestyle) ? j : 4 + j - 1)
+      col = parse.(Float64, data[:, col_idx])
+      plot!(
+        plt,
+        positions,
+        col,
+        label = "$group_name $base",
+        color = colors[base],
+        linestyle = linestyle,
+      )
+    end
   end
 
   if outfile !== nothing
@@ -188,7 +222,7 @@ function compare_fasta_files(
   frac = round(diffs / L, digits = 4)
   println("Positions with differing ATGC distributions: $diffs / $L (fraction=$frac)")
 
-  write_csv(csv_out, fname(modern), fname(ancient), comp_modern, comp_ancient)
+  write_csv_dual(csv_out, fname(modern), fname(ancient), comp_modern, comp_ancient)
   println("CSV written to $csv_out")
 end
 
